43ebf47a633e33d87e2739afef95cf5cf694261a
ReeceLangerock <ReeceLangerock@gmail.com>
2017-09-05 23:13:56 -0500

    completely style and reconfigure recipeContainer component layout

--- src/components/RecipeContainer.js
+++ src/components/RecipeContainer.js
@@ -1,9 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import Recipe from "./Recipe";
+import AddRecipe from "./AddRecipe";
+
+import { connect } from "react-redux";
+
+import * as actions from "../redux/actions/actions";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+  width: 95%;
+
+  margin: 0 auto;
+  justify-content: flex-start;
+  position: relative;
+  box-sizing: border-box;
+
+`;
 
 export class RecipeContainer extends React.Component {
+  componentWillMount(){
+    this.props.populateFromLocalStorage();
+  }
+  renderRecipeItems() {
+    return this.props.recipes.map((recipe, index) => {
+      return <Recipe key={`recipe${index}`} name={recipe.name} ingredients={recipe.ingredients} index={index} />;
+    });
+  }
   render() {
-    return <div>RecipeContainer</div>;
+    return <Container><AddRecipe/>{this.renderRecipeItems()}</Container>;
   }
 }
-export default RecipeContainer;
+
+const mapStateToProps = state => ({
+  recipes: state.recipeReducer.recipes
+});
+export default connect(mapStateToProps, actions)(RecipeContainer);
